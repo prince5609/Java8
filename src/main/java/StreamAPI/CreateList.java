f190e69fb955694640e0829af475acba363f2697
PRINCE <prince5609>
2022-10-16 17:46:40 +0530

    Stream Api

---
+++ src/main/java/StreamAPI/CreateList.java
@@ -0,0 +1,10 @@
+package StreamAPI;
+
+import java.util.List;
+
+public class CreateList {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1,2,3,4,5,6,7,8,9); // We can't modify it later
+
+    }
+}
