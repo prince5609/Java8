c6cae70d483470879c63f9e432dd1a6e1e8db9a4
PRINCE <prince5609>
2022-10-17 16:48:14 +0530

    Stream Api

--- src/main/java/StreamAPI/ObjectOfStream.java
+++ src/main/java/StreamAPI/ObjectOfStream.java
@@ -1,9 +1,13 @@
 package StreamAPI;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ObjectOfStream {
     public static void main(String[] args) {
+
         // 1 --> Blank
         Stream<Object> empty = Stream.empty();
 
@@ -13,5 +17,22 @@ public class ObjectOfStream {
         stream1.forEach(e->{
             System.out.println(e);
         });
+
+        // 3 --> List, Set
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+
+        Stream<Integer> stream2 = list1.stream();
+        List<Integer> list = stream2.filter(e->e > 0).collect(Collectors.toList());
+        System.out.println(list);
+        // can also be written as stream2.forEach(System.out::println)
+
+//        list1.stream().forEach(e->{
+//            System.out.println(e);
+//        });
     }
 }
